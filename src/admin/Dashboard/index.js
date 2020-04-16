c59f7c62d3f8c8c10e58d5ea4ac9d6306918310d
Josiah Roa <josiahroa18@gmail.com>
2020-04-16 02:47:20 -0600

    Began working on Spotify API

--- src/admin/Dashboard/index.js
+++ src/admin/Dashboard/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import spotifyAuth from '../../utils/spotifyAuth';
+import axios from 'axios';
+
 
 export default () => {
+    const handleUpdate = () => {
+        const spotifyToken = window.localStorage.getItem('spotifyToken');
+        const admin = true;
+        if(spotifyToken){
+            const artist_id = '7Md5xGlvby3sPI2NLkbYlv';
+            axios.get(`https://api.spotify.com/v1/artists/${artist_id}/albums`, 
+            {
+                headers: {
+                    'Authorization': 'Bearer ' + spotifyToken
+                }
+            })
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log(err.response.status);
+                if(err.response.status === 401){
+                    window.localStorage.removeItem(spotifyToken);
+                    window.location.href = spotifyAuth(admin);
+                }
+            })
+        }else{
+            window.location.href = spotifyAuth(admin);
+        }
+    }
+    
     return (
         <div>
             <h1>dashboard</h1>
+            <button onClick={handleUpdate}>Update Spotify</button>
         </div>
     );
 }
--- src/utils/spotifyAuth.js
+++ src/utils/spotifyAuth.js
@@ -1,6 +1,5 @@
 const authEndpoint = "https://accounts.spotify.com/authorize";
 const clientId = 'c7499dcc8400439d8db3799a33de0f08';
-const redirectUri = process.env.PUBLIC_URL || 'http://localhost:3000/callback/';
 
 const scopes = [
     "streaming",
@@ -16,6 +15,13 @@ const scopes = [
 ]
 
 // Returns a query string to be sent to if user is not authenticated with spotify
-export default () => {
+export default (admin = false) => {
+    let redirectUri;
+    if(admin){
+        redirectUri = process.env.ADMIN_URL || 'http://localhost:3000%2Fadmin%2Fdashboard%2F';
+    }else{
+        redirectUri = process.env.PUBLIC_URL || 'http://localhost:3000/callback/';
+    }
+    console.log(redirectUri);
     return `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${encodeURIComponent(scopes)}&response_type=token&show_dialog=true`
 }
