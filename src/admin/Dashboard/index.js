8a2a1b69e2d9523e79fa2767eb139955040bef58
Josiah Roa <josiahroa18@gmail.com>
2020-04-23 01:12:39 -0600

    Finished collecting necessary data from spotify and sending to backend

--- src/admin/Dashboard/index.js
+++ src/admin/Dashboard/index.js
@@ -19,10 +19,15 @@ export default () => {
             // Main data arrays
             const albums = [];
             const artists = [];
+            const tracks = [];
+            const albumArtists = [];
+            const albumTracks = [];
+            const trackArtists = [];
 
             // Helper arrays
             const foundArtists = [];
 
+            // ================= Get data for albums =================
             await axios.get(`https://api.spotify.com/v1/artists/${artist_id}/albums`, {
                 headers: {
                     'Authorization': 'Bearer ' + spotifyToken
@@ -30,40 +35,87 @@ export default () => {
             })
             .then(res => {
                 const data = res.data.items;
-                console.log(data);
 
-                // Get data for albums
                 for(let i=0; i<data.length; i++){
                     for(let j=0; j<data[i].artists.length; j++){
                         if(!foundArtists.includes(data[i].artists[j].id)){
                             foundArtists.push(data[i].artists[j].id);
                         }
+
+                        // Get data for albumId with artistId
+                        albumArtists.push({
+                            albumId: data[i].id,
+                            artistId: data[i].artists[j].id
+                        })
                     }
                     albums.push({
                         id: data[i].id,
                         name: data[i].name,
                         releaseDate: data[i].release_date,
                         imgUrl: data[i].images[1].url,
-                        externalUrl: data[i],
+                        externalUrl: data[i].external_urls.spotify,
                         privateUrl: data[i].href
                     })
                 }
             })
             .catch(err => {
-                console.log(err);
                 if(err.response.status === 401){
                     window.localStorage.removeItem(spotifyToken);
                     window.location.href = spotifyAuth();
                 }
             })
 
-            // Get data for artists
+            // ================= Get data for tracks =================
+            for(let i=0; i<albums.length; i++){
+                await axios.get(`${albums[i].privateUrl}/tracks`, {
+                    headers: {
+                        'Authorization': 'Bearer ' + spotifyToken
+                    }
+                })
+                .then(res => {
+                    const tracksRes = res.data.items;
+                    for(let j=0; j<tracksRes.length; j++){
+
+                        // Get data for albumId with trackId
+                        albumTracks.push({
+                            albumId: albums[i].id,
+                            trackId: tracksRes[j].id
+                        })
+
+                        // Get data for trackId with artistId
+                        for(let k=0; k<tracksRes[j].artists.length; k++){
+                            if(!foundArtists.includes(tracksRes[j].artists[k].id)){
+                                foundArtists.push(tracksRes[j].artists[k].id);
+                            }
+                            trackArtists.push({
+                                trackId: tracksRes[j].id,
+                                artistId: tracksRes[j].artists[k].id
+                            })    
+                        }
+
+                        tracks.push({
+                            id: tracksRes[j].id,
+                            name: tracksRes[j].name,
+                            externalUrl: tracksRes[j].external_urls.spotify,
+                            privateUrl: tracksRes[j].href
+                        })
+                    }
+                })
+                .catch(err => {
+                    if(err.response.status === 401){
+                        window.localStorage.removeItem(spotifyToken);
+                        window.location.href = spotifyAuth();
+                    }
+                })
+            }
+
+            // ================= Get data for artists =================
             let artistsQueryString = 'https://api.spotify.com/v1/artists?ids=';
             for(let i=0; i<foundArtists.length; i++){
                 artistsQueryString = artistsQueryString.concat(foundArtists[i], ',');
             }
             artistsQueryString = artistsQueryString.substring(0, artistsQueryString.length - 1);
-            
+
             await axios.get(artistsQueryString, {
                 headers: {
                     'Authorization': 'Bearer ' + spotifyToken
@@ -71,7 +123,6 @@ export default () => {
             })
             .then(res => {
                 const artistsRes = res.data.artists;
-                console.log(artistsRes);
                 for(let i=0; i<artistsRes.length; i++){
                     artists.push({
                         id: artistsRes[i].id,
@@ -90,17 +141,23 @@ export default () => {
                 }
             })
 
-            // Get data for tracks
-            // Get data for albumId with artistId
-            // Get data for albumId with trackId
-            // Get data for trackId with artistId
-
             const compiled = {
                 albums,
-                artists
+                artists,
+                tracks,
+                albumArtists,
+                albumTracks,
+                trackArtists
             }
 
-            console.log(compiled);
+            // ================= Handle post request to backend =================
+            axios.post(`http://localhost:5000/spotify/data`, compiled)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+            })
 
         }else{
             window.location.href = spotifyAuth();
