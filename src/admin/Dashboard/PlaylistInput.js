9745ac8bf131e7baa0bab57f19578f2b7044463d
Josiah Roa <josiahroa18@gmail.com>
2020-05-17 12:01:18 -0600

    Worked on sending playlist data to backend

--- src/admin/Dashboard/PlaylistInput.js
+++ src/admin/Dashboard/PlaylistInput.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export default () => {
+export default (props) => {
     const [ link, setLink ] = useState('');
 
     const handleChange = e => {
         setLink(e.target.value);
     }
 
-    const handleUpdate = () => {
-        axios.post('http://localhost:5000/playlists', {link})
+    // TODO: Validate Playlist URL
+    const handleUpdate = e => {
+        axios.put('http://localhost:5000/playlists', {
+            link,
+            id: e.target.id
+        })
         .then(res => {
             console.log(res);
         })
@@ -17,16 +21,20 @@ export default () => {
             console.log(err);
         })
     }
+    
+    useEffect(() => {
+        setLink(props.url);
+    }, [])
 
     return (
         <form>
             <input
-                name='link'
+                name={`link${props.id}`}
                 type='text'
                 onChange={handleChange}
                 value={link}
             />
-            <div onClick={handleUpdate} className='update-btn'>
+            <div onClick={handleUpdate} className='update-btn' id={props.id}>
                 <p>Update</p>
             </div>
         </form>
--- src/components/Home/Playlists/Playlists.js
+++ src/components/Home/Playlists/Playlists.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { PlaylistContainer } from '../styles';
 import playlistBg1 from '../../../assets/playlist-bg-1.svg';
 import playlistBg2 from '../../../assets/playlist-bg-2.svg';
+import axios from 'axios';
 
 export default () => {
+    const [ playlists, setPlaylists ] = useState({});
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/playlists')
+        .then(res => {
+            res.data.sort((a, b) => {
+                return a.id - b.id;
+            })
+            console.log(res.data);
+            setPlaylists(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, []);
+
     return (
         <PlaylistContainer>
             <img src={playlistBg1} id='bg1' alt=''/>
             <img src={playlistBg2} id='bg2' alt=''/>
-            <h1>Playlists</h1>
+            <div className='content-container'>
+                <h1>Playlists</h1>
 
+            </div>
         </PlaylistContainer>
     )
 }
