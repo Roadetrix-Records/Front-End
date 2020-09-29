f2d942450ca729b1ad60923801878bfced9d1c13
Josiah Roa <josiahroa18@gmail.com>
2020-09-28 23:51:34 -0600

    Added new theme file

---
+++ src/newTheme.js
@@ -0,0 +1,27 @@
+export default {
+    colors: {
+        primary: {
+            spotifyGreen: '#2AB052'
+        },
+        neutral: {
+            black: '#1D1D1D',
+            white: '#FFFFFF'
+        },
+        gradient: {
+            darkGradient: 'background: linear-gradient(0deg, rgba(29,29,29,1) 0%, rgba(0,0,0,1) 100%)'
+        }
+    },
+    font: {
+        family: 'Ubuntu, sans-serif',
+        sizing: {
+            h1: '',
+            h2: '',
+            h3: '',
+            text: '',
+            smText: ''
+        }
+    },
+    breakpoints: {
+        
+    }
+}
